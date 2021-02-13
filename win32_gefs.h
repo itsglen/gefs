d07af8a5d3d077e0b3ee56e2bca5a28d60e853ac
stipanciktomislav <stipanciktomislav@gmail.com>
2021-02-12 16:34:00 -0800

    Update comment

--- win32_gefs.h
+++ win32_gefs.h
@@ -6,7 +6,7 @@ struct win32_window_dimension
 
 struct win32_offscreen_buffer
 {
-    // NOTE(tomi): Pixels are alwasy 32-bits wide, Memory Order BB GG RR XX
+    // NOTE(tomi): Pixel memory order -> BB GG RR XX
     BITMAPINFO Info;
     void *Memory;
     int Width;
